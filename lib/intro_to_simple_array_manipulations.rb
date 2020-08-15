ffd7d241a766bb0c479a15d587a3c197de6e02a4
Victor Romero <romeroa.victor@yahoo.com>
2020-08-15 15:38:00 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -2,8 +2,7 @@ def using_concat(array1, array2)
   array1.concat(array2)
 end
 
-def using_insert(array, element)
-  new_string = ""
+def using_insert(array, element, new_string)
   element.insert(array[4], new_string)
 end
 
